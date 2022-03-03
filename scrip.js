0810c08a9817575d89a9411612826a58def3f7af
Hafidhmj95 <hafidhmuhammadjulda@gmail.com>
2022-03-04 01:41:36 +0700

    commit pertama

--- scrip.js
+++ scrip.js
@@ -1,6 +1,5 @@
 const nilai = document.getElementsByClassName("item");
 const inputBox = document.getElementById("input");
-const reset = document.getElementById("ulang");
 let angka1 = 0;
 let angka2 = 0;
 let operasi = "";
@@ -8,38 +7,30 @@ let operasi = "";
 
 for(let i = 0; i < nilai.length; i++){
     nilai[i].addEventListener("click", function(){
-        if(nilai[i].innerText === "+"){
-        if(nilai[i].innerText === "-"){
-        if(nilai[i].innerText === "*"){
-        if(nilai[i].innerText === "/"){ 
-
-            operasi = nilai[i].innerText;
+        if(nilai[i].innerText === "+"){             
             angka1 = inputBox.value;
+            operasi = "+";
             inputBox.value = "0";
         } else if(nilai[i].innerText === "=") {
             angka2 = inputBox.value;
-            if(operasi === "+") 
-                inputBox.value = parseInt(angka1) + parseInt(angka2);
-            if(operasi === "-") 
-                inputBox.value = parseInt(angka1) - parseInt(angka2);
-            if(operasi === "*") 
-                inputBox.value = parseInt(angka1) * parseInt(angka2);
-            if(operasi === "/")
-            inputBox.value = parseInt(angka1) / parseInt(angka2);
+            if(operasi === "+") {
+                inputBox.value = parseInt(angka1) + parseInt(angka2)
+            }
+            if(operasi === "-") {
+                inputBox.value = parseInt(angka1) - angka1(angka2)
+            }
         } else {
-            const currentValue = parseInt(inputBox.value);
             if(inputBox.value === "0") { 
                 inputBox.value = nilai[i].innerText
             } else {
-                inputBox.value = currentValue + nilai[i].innerText;
+                inputBox.value = inputBox.value + nilai[i].innerText
             }
         }
-    });
+    })
 }
-
+const reset = document.getElementById("reset");
 reset.addEventListener("click", function(){
-    operasi = "";
-    inputBox.value = "0";
-    angka1 = 0;
-    angka2 = 0;
-});
+    inputBox.value = "0"
+    angka1 = 0
+    angka2 - 0
+})
